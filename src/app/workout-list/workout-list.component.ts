55521faa05c9a853e1893a0d4a6ae34671ddd759
felixKroni <felixkronsteiner@gmx.at>
2023-12-02 19:38:58 +0100

    nearly done

--- src/app/workout-list/workout-list.component.ts
+++ src/app/workout-list/workout-list.component.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { DatePipe } from '@angular/common';
 import { Timestamp } from "@firebase/firestore-types";
 import { Workout } from '../Model/workout.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-workout-list',
@@ -11,20 +12,25 @@ import { Workout } from '../Model/workout.model';
 })
 export class WorkoutListComponent implements OnInit {
   workouts: Workout[] = [];
+  workoutsSubscription: Subscription = new Subscription;
 
   constructor(private firestore: AngularFirestore, private datePipe: DatePipe) {}
 
   ngOnInit(): void {
-    //suggested by copilot
-    this.firestore.collection<Workout>('WorkoutTest').valueChanges().subscribe((workouts: Workout[]) => {
-      console.log("UPDATED WORKOUTS")
+    this.workoutsSubscription = this.firestore.collection<Workout>('Workouts').valueChanges().subscribe((workouts: Workout[]) => {
       this.workouts = workouts;
-      
-      console.log("WORKOUTS: " + this.workouts.toString());
+      console.log("WORKOUTS UPDATED");
     });
   }
 
-  formattedWorkoutDate(date: Date): string {
+  ngOnDestroy(): void {
+    this.workoutsSubscription.unsubscribe();
+    console.log("WORKOUTS UNSUBSCRIBED");
+  }
+
+  formattedWorkoutDate(obj: object): string {
+    const date = (obj as Timestamp).toDate();
+    console.log("FORMATTING DATE: " + date);
     let formattedDate = this.datePipe.transform(date, 'dd.MMM.yyyy HH:mm') ?? "";
     return formattedDate;
   }
@@ -33,3 +39,29 @@ export class WorkoutListComponent implements OnInit {
     console.log("WORKOUT CLICKED: " + workout.toString());
   }
 }
+
+/*
+
+
+ngOnInit(): void {
+    this.firestore.collection<Workout>('Workouts').valueChanges().subscribe((workouts: Workout[]) => {
+      console.log("UPDATED WORKOUTS")
+      this.workouts = workouts;
+      this.workouts.forEach((workout: Workout) => {
+        console.log("Workout name:"+workout.name);
+        console.log("Workout date:"+workout.date.getDate);
+      });
+    });
+  }
+
+
+  ngOnInit(): void {
+    this.firestore.collection<Workout>('Workouts').valueChanges().subscribe((workouts: Workout[]) => {
+      console.log("UPDATED WORKOUTS")
+      this.workouts = workouts.map(workout => {
+        date: workout.date.toDate,
+        
+    });
+  }
+
+*/
